59661860fc0543f9a2bd69f5be60d061981e1760
Yuri Sementsov <yuri.sementsov@ecentria.com>
2020-06-10 11:34:32 +0300

    Refactoring

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,4 @@
 source $HOME/.dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source $HOME/.dotfiles/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+source $HOME/.dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+# source $HOME/.dotfiles/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source $HOME/.dotfiles/zsh/plugins/pj/pj.plugin.zsh
