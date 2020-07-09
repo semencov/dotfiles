d74671d2af238ba7ef771dec4de30f6a146bfd61
Yuri Sementsov <yuri.sementsov@ecentria.com>
2020-07-09 18:24:09 +0300

    Install script

---
+++ setup/node.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# n, Node version manager
+n latest
+n lts
+npm config set loglevel warn
+
+# Npm
+npm i -g npm-upgrade
+npm i -g npkill
+npm i -g tldr
+
+npm i -g yarn@legacy
+npm i -g lerna
+npm i -g regexgen
+npm i -g @lhci/cli
+npm i -g pageres-cli
+npm i -g broken-link-checker
+npm i -g @fabiospampinato/bump
+
+npm i -g @vue/cli
+npm i -g gatsby-cli
+npm i -g preact-cli
+npm i -g serverless
+npm i -g @maizzle/cli
+npm i -g @gridsome/cli
+npm i -g create-nuxt-app
+npm i -g create-react-app
+
+npm i -g vercel
+npm i -g surge
+npm i -g netlify-cli
+npm i -g @zeplin/cli
+npm i -g @aws-amplify/cli
