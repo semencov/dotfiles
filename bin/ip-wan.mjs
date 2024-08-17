fb4c86967e14f48c387fb2110448a1dd514e6177
Yuri Sementsov <hello@smcv.dev>
2024-08-18 01:46:43 +0300

    SOAP is a piece of shit

---
+++ bin/ip-wan.mjs
@@ -0,0 +1,13 @@
+#!/usr/bin/env zx
+
+/**
+ * Show WAN IP
+ */
+
+try {
+	await $`dig +short myip.opendns.com @resolver1.opendns.com`;
+	echo(result);
+} catch (error) {
+	console.error("Error fetching WAN IP:", error.message);
+	process.exit(1);
+}
